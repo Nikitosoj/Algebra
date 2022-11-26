6b9977adb5b253b32f01dcf096d5d44c5d528360
Nikitosoj <nikita.kaneki@gmail.com>
2022-11-26 21:38:25 +0300

    123

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 global k
+n = 2
 def quadr(x):
     if x == 0:
         return(1)
@@ -71,28 +72,50 @@ for i in range(k):
 for i in range(k):
     for l in range(k):
         y1.append(l)
-print("x | y | max(x,y) | min(x,y) |   x*y   | tilda(x) | post(x)| I3(x)| J3(x)| x + y| x * y| minum(*_)| imp(x,y)| VV(x,y)| x -y ")
+# print("x | y | max(x,y) | min(x,y) |   x*y   | tilda(x) | post(x)| I3(x)| J3(x)| x + y| x * y| minum(*_)| imp(x,y)| VV(x,y)| x -y ")
 
 
-result = [[],[]]
+result = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
 
 final = []
+# for i in range(k*k):
+#     print(x1[i]," ", y1[i] ,"    ", max(x1[i], y1[i]),"      ", min(x1[i], y1[i]),"         ",
+#     mult(x1[i], y1[i]) , "          ", tilda(x1[i]), "        ", post(x1[i]), "    ",I(2,x1[i]),
+#     "    ",J(2,x1[i]), "   ", plus(x1[i],y1[i]), "   ", mult(x1[i],y1[i]), "     ", minum(x1[i],y1[i]), "           ",
+#      imp(x1[i],y1[i]), "     ", VV(x1[i],y1[i]), "     ", minus(x1[i],y1[i]) )
+# for i in range(k*k):
+#     result[0].append(post(x1[i]))
+# print(result[0])
+# for i in range(k*k):
+#     result[1].append(plus(result[0][i],result[0][i]))
+# print(result[1])
+# result[0] = []
+# for i in range(k*k):
+#     result[0].append(tilda(result[1][i]))
+# print(result[0])
+# result[1] = []
+# for i in range(k*k):
+#     result[1].append(mult(result[0][i],quadr(x1[i])))
+# print(result[1])
+def math(n,x,y):
+    if n == 4: return(minus(x,y))
+    elif n == 5: return(VV(x,y))
+    elif n == 6: return(imp(x,y))
+    elif n == 7: return(minum(x,y))
+    elif n == 8: return(plus(x,y))
+    elif n == 9: return(mult(x,y))
+    elif n == 10: return(J(x,y))
+    elif n == 11: return(I(x,y))
+    elif n == 12: return(max(x,y))
+    elif n == 13: return(min(x,y))
+l = 3
+def math2(n,x):
+    if n == 1: return(quadr(x))
+    elif n == 2: return(post(x))
+    elif n == 3: return(tilda(x))
 for i in range(k*k):
-    print(x1[i]," ", y1[i] ,"    ", max(x1[i], y1[i]),"      ", min(x1[i], y1[i]),"         ",
-    mult(x1[i], y1[i]) , "          ", tilda(x1[i]), "        ", post(x1[i]), "    ",I(2,x1[i]),
-    "    ",J(2,x1[i]), "   ", plus(x1[i],y1[i]), "   ", mult(x1[i],y1[i]), "     ", minum(x1[i],y1[i]), "           ",
-     imp(x1[i],y1[i]), "     ", VV(x1[i],y1[i]), "     ", minus(x1[i],y1[i]) )
-for i in range(k*k):
-    result[0].append(post(x1[i]))
-print(result[0])
-for i in range(k*k):
-    result[1].append(plus(result[0][i],result[0][i]))
-print(result[1])
-result[0] = []
-for i in range(k*k):
-    result[0].append(tilda(result[1][i]))
-print(result[0])
-result[1] = []
-for i in range(k*k):
-    result[1].append(mult(result[0][i],quadr(x1[i])))
-print(result[1])
+    if n >= 1 and n <= 3:
+        result[l].append(math2(n,x1[i]))
+    if n >=4 and n<=13:
+        result[l].append(math(n,x1[i],y1[i]))
+print(result[l])
